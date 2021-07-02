edddc9b24e63e0c1ffe2b73dcf228e3b7435806f
Marvin Sextro <marvin.sextro@gmail.com>
2021-07-02 22:50:20 +0200

    Copy config in dockerfile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import 'tailwindcss/tailwind.css';
 import {DefaultSeo} from 'next-seo';
 import SEO from '../next-seo.config';
 import type {AppProps} from 'next/app';
-import { appWithTranslation } from 'next-i18next';
+import {appWithTranslation} from 'next-i18next';
 
 function MyApp({Component, pageProps}: AppProps): JSX.Element {
     return (
