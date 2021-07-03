935e6f8227300160c028599816815cacf4b200d1
Marvin Sextro <marvin.sextro@gmail.com>
2021-07-03 13:11:43 +0200

    Finish German translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'de'],
     localeExtension: 'yml',
     defaultLanguage: 'en',
     fallbackLng: ['en'],
