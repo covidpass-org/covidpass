d1886a9b9e7bc8c6b8cb81b65411d2a62ed924e4
Marvin Sextro <marvin.sextro@gmail.com>
2021-07-15 20:25:03 +0200

    Integrate french translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de', 'de-DE', 'de-AT', 'de-LI', 'de-LU', 'de-CH'],
+    locales: [
+      'en', 
+      'de', 'de-DE', 'de-AT', 'de-LI', 'de-LU', 'de-CH',
+      'fr', 'fr-BE', 'fr-CH', 'fr-FR', 'fr-LU', 'fr-MC'
+    ],
     localeExtension: 'yml',
   },
 };
