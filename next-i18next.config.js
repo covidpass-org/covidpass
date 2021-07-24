b0babdd4c7e34eb7bc65792bbf6466caa900e745
Marvin Sextro <marvin.sextro@gmail.com>
2021-07-24 22:06:00 +0200

    Add Finnish translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,7 +5,8 @@ module.exports = {
       'en', 
       'de', 'de-DE', 'de-AT', 'de-LI', 'de-LU', 'de-CH',
       'fr', 'fr-BE', 'fr-CH', 'fr-FR', 'fr-LU', 'fr-MC',
-      'nl', 'nl-NL', 'nl-BE'
+      'nl', 'nl-NL', 'nl-BE',
+      'fi', 'fi-FI'
     ],
     localeExtension: 'yml',
   },
