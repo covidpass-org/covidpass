e2eca849728feb0a4417c3782dd0325e7b0fbd98
Ryan Slobojan <straxus@gmail.com>
2021-10-14 16:45:35 -0400

    Added second QC Key ID
    * Apparently QC has been changing things around a bit:
    https://github.com/fproulx/shc-covid19-decoder/issues/12#issuecomment-932668689

--- src/issuers.js
+++ src/issuers.js
@@ -8,6 +8,10 @@ export const issuers = [
         alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
         x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
         y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0" },
+      { kid: "2XlWk1UQMqavMtLt-aX35q_q9snFtGgdjH4-Y1gfH1M",
+        alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
+        x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
+        y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0" },
     ]
   },
   {
