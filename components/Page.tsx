32cb366b1799adea606e7347f7993f14bf804789
Jason Liu <jasonisaliu@gmail.com>
2021-10-21 12:28:36 -0400

    removed verifier and demo link

--- components/Page.tsx
+++ components/Page.tsx
@@ -27,10 +27,8 @@ function Page(props: PageProps): JSX.Element {
                     <footer>
                         <nav className="nav flex pt-4 flex-row space-x-4 justify-center text-md font-bold flex-wrap">
                             {<Link href="/faq"><a className="underline">{t('common:faq')}</a></Link>}
-                            <a href="https://www.youtube.com/watch?v=AIrG5Qbjptg" className="underline">{t('index:demo')}</a>
                             <a href="https://twitter.com/grassroots_team" className="underline">{t('index:whatsnew')}</a>
                             <a href="mailto:grassroots@vaccine-ontario.ca" className="underline">{t('common:contact')}</a>
-                            <a href="https://verifier.vaccine-ontario.ca" className="underline">{t('common:gotoVerifier')}</a>
                             <a href="https://github.com/billylo1/covidpass" className="underline">{t('common:gitHub')}</a>
                             <a href="https://vaccine-ontario.ca" className="underline">{t('common:returnToMainSite')}</a>
                         </nav>
