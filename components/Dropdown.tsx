df5f72e40b338e2f9b313a34d8f20f12c924a40f
Jason Liu <jasonisaliu@gmail.com>
2021-10-20 22:33:40 -0400

    created tailwind dropdown component
    removed react-select
    reenabled dark mode

---
+++ components/Dropdown.tsx
@@ -0,0 +1,59 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+// import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+interface DropdownProps {
+    label: string,
+    options: { label: string, value: string }[]
+}
+
+export default function Dropdown(props: DropdownProps) {
+    const { label, options } = props;
+    return (
+        <Menu as="div" className="relative inline-block text-left w-full">
+            <div>
+                <Menu.Button className="inline-flex justify-center w-full focus:outline-none bg-green-600 py-2 px-3 text-white font-semibold rounded-md disabled:bg-gray-400">
+                    {label}
+                    <svg className=" h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 10" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="origin-top-left w-full absolute left-0 mt-2 rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                        {options.map(option => (
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a
+                                        href={option.value}
+                                        className={classNames(
+                                            active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900' : 'text-gray-700',
+                                            'dark:text-white block px-4 py-2'
+                                        )}
+                                    >
+                                        {option.label}
+                                    </a>
+                                )}
+                            </Menu.Item>
+                        ))}
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
+}
--- components/Form.tsx
+++ components/Form.tsx
@@ -15,8 +15,7 @@ import {isIOS, isMacOs, isAndroid, isSafari, osVersion, getUA, browserName, brow
 import * as Sentry from '@sentry/react';
 import Bullet from './Bullet';
 import { GPayData } from '../src/gpay';
-
-import Select from 'react-select'
+import Dropdown from './Dropdown';
 
 const options = [
     { label: 'Alberta', value: 'https://covidrecords.alberta.ca/form'},
@@ -58,8 +57,6 @@ function Form(): JSX.Element {
     const [fileErrorMessages, _setFileErrorMessages] = useState<Array<string>>([]);
 
     const [showDoseOption, setShowDoseOption] = useState<boolean>(false);
-
-    const [selectedOption, setSelectedOption] = useState(null);
 
     // const [warningMessages, _setWarningMessages] = useState<Array<string>>([]);
     const hitcountHost = 'https://stats.vaccine-ontario.ca';
@@ -518,24 +515,12 @@ function Form(): JSX.Element {
         // }
     }
 
-    function gotoLink(option) {
-        window.location.href = option.value;
-    }
-
     return (
         <div>
             <form className="space-y-5" id="form" onSubmit={addToWallet}>
                 <Card step="1" heading={t('index:downloadReceipt')} content={
                     <div className="space-y-5">
-                        <p>
-                            <div>If you need to download your proof-of-vaccination, please select your province in the drop-down to be redirected to your provincial proof-of-vaccination portal.<br/> <b>IF YOU HAVE YOUR PROOF-OF-VACCINATION, SKIP THIS STEP AND PROCEED DIRECTLY TO STEP 2</b></div>
-                            <Select
-                                className="dark"
-                                defaultValue={selectedOption}
-                                onChange={e => { setSelectedOption; gotoLink(e)}}
-                                options={options}
-                            />
-                        </p>
+                        <Dropdown label="Select Your Province" options={options} />
                     </div>
                 }/>
 
