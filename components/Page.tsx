67a4a1500780cf76fa7d37971fbcd5519d3f4b2d
Ryan Slobojan <straxus@gmail.com>
2021-10-23 18:51:36 -0400

    Added new provinces and states to the list of accepted regions
    * Added links to NL and PEI proof-of-vaccination portals
    * Fixed Wallet and Pay buttons not looking disabled when inactive
    * Bumped version to 2.2.1
    * Some dead code removal and warning cleanup
    * We need to find a better way to list regions - it's turning into an eternal run-on sentence, hard to identify supported regions

--- components/Page.tsx
+++ components/Page.tsx
@@ -32,7 +32,7 @@ function Page(props: PageProps): JSX.Element {
                             <a href="https://github.com/billylo1/covidpass" className="underline">{t('common:gitHub')}</a>
                             <a href="https://vaccine-ontario.ca" className="underline">{t('common:returnToMainSite')}</a>
                         </nav>
-                        <div className="flex pt-4 flex-row space-x-4 justify-center text-md flex-wrap">Last updated: 2021-10-19 (v2.2.0)</div>
+                        <div className="flex pt-4 flex-row space-x-4 justify-center text-md flex-wrap">Last updated: 2021-10-23 (v2.2.1)</div>
                     </footer>
                 </main>
             </div>
