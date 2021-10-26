215ac153d1cb14b900fe90b107285aedf66e5515
Billy Lo <billylo1@gmail.com>
2021-10-26 18:01:55 -0400

    switch to sponsored account

--- src/sentry.ts
+++ src/sentry.ts
@@ -4,7 +4,7 @@ import { Integrations } from '@sentry/tracing';
 export const initSentry = () => {
     SentryModule.init({
         release: 'grassroots_covidpass@2.2.5', // App version. Needs to be manually updated as we go unless we make the build smarter
-        dsn: 'https://7120dcf8548c4c5cb148cdde2ed6a778@o1015766.ingest.sentry.io/5981424',
+        dsn: 'https://51370d7af0994761b465bc148129c1de@o997324.ingest.sentry.io/5955697',  // this is the sponsored account for use going forward
         integrations: [
             new Integrations.BrowserTracing(),
         ],
