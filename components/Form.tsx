d2054446595a58818818797389241ee928270ccd
Marvin Sextro <marvin.sextro@gmail.com>
2022-01-10 04:04:29 +0100

    Try to fix buttons

--- components/Form.tsx
+++ components/Form.tsx
@@ -13,7 +13,7 @@ import {getPayloadBodyFromFile, getPayloadBodyFromQR} from "../src/process";
 import {PassData} from "../src/pass";
 import {COLORS} from "../src/colors";
 import Colors from './Colors';
-import Button, { ButtonType } from './Button';
+import Button from './Button';
 
 function Form(): JSX.Element {
     const {t} = useTranslation(['index', 'errors', 'common']);
@@ -278,7 +278,19 @@ function Form(): JSX.Element {
                             </p>
                         </label>
                         <div className="grid grid-cols-1">
-                            <Button type={ButtonType.submit} text={t('index:addToWallet')} loading={loading} />
+                            <button 
+                                type="submit"
+                                className="bg-green-600 hover:bg-green-700 relative focus:outline-none h-20 text-white font-semibold rounded-md items-center flex justify-center">
+                                <div id="spin" className={`${loading ? undefined : "hidden"} absolute left-2`}>
+                                    <svg className="animate-spin h-5 w-5 ml-4" viewBox="0 0 24 24">
+                                        <circle className="opacity-0" cx="12" cy="12" r="10" stroke="currentColor"
+                                                strokeWidth="4"/>
+                                        <path className="opacity-80" fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                    </svg>
+                                </div>
+                                {t('index:addToWallet')}
+                            </button>
                         </div>
                     </div>
                 }/>
